4078a3fe120760d741109acf552ebc5b85859db7
Vadim <vafilo@taltech.ee>
2025-03-04 14:11:47 +0200

    Track the number of user clicks

--- clicker-service/src/test/java/com/vcasino/clicker/service/ClickerServiceTest.java
+++ clicker-service/src/test/java/com/vcasino/clicker/service/ClickerServiceTest.java
@@ -12,6 +12,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.kafka.core.KafkaTemplate;
 
 import java.math.BigDecimal;
 import java.sql.Timestamp;
@@ -31,6 +32,9 @@ public class ClickerServiceTest {
 
     @Mock
     private AccountService accountService;
+
+    @Mock
+    private KafkaTemplate<String, Object> kafkaTemplate;
 
     @InjectMocks
     ClickerService clickerService;
