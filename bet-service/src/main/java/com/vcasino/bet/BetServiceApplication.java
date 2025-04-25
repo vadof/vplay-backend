2fe52dba102d6f9626c3f8882d258150d1f2ef03
Vadim <vafilo@taltech.ee>
2025-04-26 00:41:08 +0300

    Add @EnableScheduling to BetService

--- bet-service/src/main/java/com/vcasino/bet/BetServiceApplication.java
+++ bet-service/src/main/java/com/vcasino/bet/BetServiceApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableScheduling
 public class BetServiceApplication {
 
     public static void main(String[] args) {
